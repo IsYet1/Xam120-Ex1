e6b69c6146ce2cfcd19e97893444f04fd01a4572
IsYet1 <Don@IsYet.com>
2016-08-08 17:16:21 -0500

    Added layout, entry, buttons, etc. Matches the style in the lesson.

--- PhoneWord/PhoneWord/PhoneWord/MainPage.cs
+++ PhoneWord/PhoneWord/PhoneWord/MainPage.cs
@@ -2,19 +2,50 @@
 
 public class MainPage : ContentPage
 {
+    Entry _entry;
+    Button _translateBtn;
+    Button _callBtn;
+
     public MainPage()
     {
-        Content = new StackLayout
-        {
-            VerticalOptions = LayoutOptions.Center,
-            Children =
-            {
-                new Label
-                {
-                    HorizontalTextAlignment = TextAlignment.Center,
-                    Text = "Welcome to Xamarin Forms!"
-                }
-            }
+        this.Padding = new Thickness(20,
+            Device.OnPlatform<double>(40,20,20),20,20);
+
+        StackLayout panel = new StackLayout {
+            VerticalOptions = LayoutOptions.FillAndExpand,
+            HorizontalOptions = LayoutOptions.FillAndExpand,
+            Orientation = StackOrientation.Vertical,
+            Spacing = 15,
         };
+
+        panel.Children.Add(
+            new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Start,
+                Text = "Enter a Phoneword"
+            }
+        );
+
+        panel.Children.Add(
+            _entry = new Entry { Text = "1-855-Xamarin"}
+        );
+
+        panel.Children.Add(
+            _translateBtn = new Button
+            {
+                Text = "Translate"
+            }
+        );
+
+        panel.Children.Add(
+            _callBtn = new Button
+            {
+                Text = "Call",
+                IsEnabled = false
+            }
+        );
+
+        this.Content = panel;
+
     }
 }
