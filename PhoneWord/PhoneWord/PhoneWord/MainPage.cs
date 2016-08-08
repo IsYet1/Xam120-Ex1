85a49dd8edad86432a1f57e1d52401d43ee020f0
IsYet1 <Don@IsYet.com>
2016-08-08 16:38:39 -0500

    Refactor MainPage to a class. Builds and runs in UWP.

---
+++ PhoneWord/PhoneWord/PhoneWord/MainPage.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+public class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        Content = new StackLayout
+        {
+            VerticalOptions = LayoutOptions.Center,
+            Children =
+            {
+                new Label
+                {
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    Text = "Welcome to Xamarin Forms!"
+                }
+            }
+        };
+    }
+}
