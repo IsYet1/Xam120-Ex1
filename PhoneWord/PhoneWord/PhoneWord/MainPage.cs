42b5a6a548a427b4fc6ba512da15d50b17d0f728
IsYet1 <Don@IsYet.com>
2016-08-08 17:25:40 -0500

    Revert "Change namespace"
    This reverts commit 74828158e513780541d481f21a8da29be00cb0b9.

--- PhoneWord/PhoneWord/PhoneWord/MainPage.cs
+++ PhoneWord/PhoneWord/PhoneWord/MainPage.cs
@@ -1,65 +1,51 @@
 ﻿using Xamarin.Forms;
 
-
-namespace PhoneWord
+public class MainPage : ContentPage
 {
-    public class MainPage : ContentPage
+    Entry _entry;
+    Button _translateBtn;
+    Button _callBtn;
+
+    public MainPage()
     {
-        Entry _entry;
-        Button _translateBtn;
-        Button _callBtn;
-        string _translatedNumber;
+        this.Padding = new Thickness(20,
+            Device.OnPlatform<double>(40,20,20),20,20);
 
-        public MainPage()
-        {
-            this.Padding = new Thickness(20,
-                Device.OnPlatform<double>(40,20,20),20,20);
+        StackLayout panel = new StackLayout {
+            VerticalOptions = LayoutOptions.FillAndExpand,
+            HorizontalOptions = LayoutOptions.FillAndExpand,
+            Orientation = StackOrientation.Vertical,
+            Spacing = 15,
+        };
 
-            StackLayout panel = new StackLayout {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                Orientation = StackOrientation.Vertical,
-                Spacing = 15,
-            };
+        panel.Children.Add(
+            new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Start,
+                Text = "Enter a Phoneword"
+            }
+        );
 
-            _translateBtn.Clicked += OnTranslate;
+        panel.Children.Add(
+            _entry = new Entry { Text = "1-855-Xamarin"}
+        );
 
-            panel.Children.Add(
-                new Label
-                {
-                    HorizontalTextAlignment = TextAlignment.Start,
-                    Text = "Enter a Phoneword"
-                }
-            );
+        panel.Children.Add(
+            _translateBtn = new Button
+            {
+                Text = "Translate"
+            }
+        );
 
-            panel.Children.Add(
-                _entry = new Entry { Text = "1-855-Xamarin"}
-            );
+        panel.Children.Add(
+            _callBtn = new Button
+            {
+                Text = "Call",
+                IsEnabled = false
+            }
+        );
 
-            panel.Children.Add(
-                _translateBtn = new Button
-                {
-                    Text = "Translate"
-                }
-            );
+        this.Content = panel;
 
-            panel.Children.Add(
-                _callBtn = new Button
-                {
-                    Text = "Call",
-                    IsEnabled = false
-                }
-            );
-
-            this.Content = panel;
-
-        }
-
-        private void OnTranslate(object sender, System.EventArgs e)
-        {
-            var translateThis = _entry.Text;
-            var translated = PhonewordTranslator.ToNumber(translateThis);
-
-        }
     }
 }
